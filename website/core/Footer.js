76be1bcd8fcfdd2ecf6a822308dd7ddba5b5da24
clovisd <clovisd@users.noreply.github.com>
2019-06-30 04:23:35 -0600

    Update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -35,46 +35,41 @@ class Footer extends React.Component {
               />
             )}
           </a>
-          <div>
-            <h5>Docs Quick Start:</h5>
-            <a href={this.docUrl('ba.html', this.props.language)}>
-              BattleArena Docs
-            </a>
-            <a href={this.docUrl('bt.html', this.props.language)}>
-              BattleTracker Docs
-            </a>
-            <a href={this.docUrl('dev.html', this.props.language)}>
-              API and Development Docs
-            </a>
-            <a href={this.docUrl('other.html', this.props.language)}>
-              Other Plugins & Docs
-            </a>
-          </div>
-          <div>
+          <div align="left">
             <h5>Organization Info:</h5>
-            <a href="https://discord.gg/tMVPVJf"><i class="fab icon-white fa-discord"/> Discord Support</a>
-            <a href="https://github.org/BattlePlugins"><i class="fab icon-white fa-github-square"/> Github Org</a>
-            <a href="https://battleplugins.org/"><i class="fas icon-white fa-desktop"/> Website</a>
+            <a href="https://battleplugins.org/"><img src="https://battleplugins.org/img/favicon.ico" alt="Icon" height="17"/> BattlePlugins Site</a>
+            <a href="https://discord.gg/tMVPVJf"><img src="https://battleplugins.org/img/discord.ico" alt="Icon" height="17"/>  Discord Server</a>
+            <a href="https://github.org/BattlePlugins"><img src="https://battleplugins.org/img/night.ico" alt="Icon" height="17"/> Github Repos</a>
+            <a href="https://ci.battleplugins.org/"><img src="https://battleplugins.org/img/jenkins.ico" alt="Icon" height="17"/> Jenkins CI</a>
+            <a href="https://repo.battleplugins.org/"><img src="https://battleplugins.org/img/repo.ico" alt="Icon" height="17"/> Public Repo</a>
+          </div>
+          <div align="left">
+            <h5>Docs Quick Start:</h5>
+            <a href="https://docs.battleplugins.org/docs/"><i class="fas icon-white fa-desktop"/> Docs Home</a>
+            <a href="https://docs.battleplugins.org/docs/ba"><i class="fas icon-white fa-border-style"/> BattleArena Docs</a>
+            <a href="https://docs.battleplugins.org/docs/bt"><i class="fas icon-white fa-chart-area"/> BattleTracker Docs</a>
+            <a href="https://docs.battleplugins.org/docs/vp"><i class="fas icon-white fa-user-friends"/> VirtualPlayer Docs</a>
             <a href="https://wiki.battleplugins.org/"><i class="fas icon-white fa-scroll"/> Legacy Wiki</a>
           </div>
-          <div>
-            <h5>About the Docs:</h5>
-            <a href="https://github.com/BattlePlugins/BattleDocs">Contribute to the Docs Today</a>
-            <a href="http://ci.battleplugins.org/jobs/BattleDocs">Build Status: <img alt="Jenkins" src="https://img.shields.io/jenkins/build/http/s14.cl6.us:8080/job/BattleDocs.svg?style=flat-square"/></a>
-            <a href="https://github.com/BattlePlugins/BattleDocs">Doc Size: <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/battleplugins/battledocs.svg?style=flat-square"/></a>
-            <a href="https://github.com/BattlePlugins/BattleDocs/pulls">Pending Updates: <img alt="GitHub pull requests" src="https://img.shields.io/github/issues-pr/battleplugins/battledocs.svg?style=flat-square"/></a>
-            <br></br>
-            <img alt="GitHub followers" src="https://img.shields.io/github/followers/battleplugins.svg?label=Follow&style=social"/> | <img alt="GitHub stars" src="https://img.shields.io/github/stars/battleplugins/battledocs.svg?style=social"/> | <img alt="GitHub watchers" src="https://img.shields.io/github/watchers/battleplugins/battledocs.svg?label=Watch&style=social"/>
+          <div align="left">
+            <h5>Site Stats:</h5>
+            <a href="https://battleplugins.org" className="footer-badge"><img alt="Jenkins Builds" src="https://img.shields.io/jenkins/build/https/ci.battleplugins.org/job/BattlePlugins/job/battledocs/job/master.svg?label=Build%20Status&logo=jenkins&logoColor=white&style=flat-square"/></a>
+            <a href="https://battleplugins.org" className="footer-badge"><img alt="Discord Server" src="https://img.shields.io/discord/305170628468801537.svg?color=brightgreen&label=Discord%20Server&logo=discord&logoColor=white&style=flat-square"/></a>
+            <a href="https://battleplugins.org" className="footer-badge"><img alt="GitHub Watchers" src="https://img.shields.io/github/watchers/battleplugins/battledocs.svg?color=brightgreen&label=Watching%20Repo%3A&logo=github&logoColor=white&style=flat-square"/></a>
+            <a href="https://battleplugins.org" className="footer-badge"><img alt="GitHub Repo Size" src="https://img.shields.io/github/repo-size/battleplugins/battledocs.svg?color=brightgreen&label=Site%20Size&logo=github&logoColor=white&style=flat-square"/></a>
+            <a href="https://battleplugins.org" className="footer-badge"><img alt="Most Recent Commit" src="https://img.shields.io/github/last-commit/battleplugins/battledocs.svg?label=Last%20Update&logo=github&logoColor=white&style=flat-square"/></a>
           </div>
         </section>
 		<div>
 			<center>
-			<h5 className="footer-slogan">Hosted By:</h5>
-        	<a href="https://cl6web.com" className="footer-slogan"><img src={`${this.props.config.baseUrl}img/cl6.png`} alt="CL6 Hosting" height="60"/></a>
-        	</center>
-        </div>
-        <section className="copyright">{this.props.config.copyright}</section>
-      </footer>
+        <a href="https://github.com/BattlePlugins/battledocs" className="footer-social"><img alt="GitHub Stars" src="https://img.shields.io/github/stars/battleplugins/battledocs.svg?style=social"/><a/> | <a href="https://github.com/BattlePlugins/battledocs" className="footer-social"><img alt="GitHub Watchers" src="https://img.shields.io/github/watchers/battleplugins/battledocs.svg?label=Watch&style=social"/></a>
+        <br></br>
+			     <h5 className="footer-slogan">Hosted By:</h5>
+           <a href="https://cl6web.com" className="footer-slogan"><img src={`${this.props.config.baseUrl}img/cl6.png`} alt="CL6 Hosting" height="60"/></a>
+      </center>
+    </div>
+    <section className="copyright">{this.props.config.copyright}</section>
+  </footer>
     );
   }
 }
